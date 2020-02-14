39627767c6a95a36d032dbbe31dea6b1b5d24320
jiwon824 <59115860+jiwon824@users.noreply.github.com>
2020-02-14 21:36:33 +0900

    Add files via upload

---
+++ python_1day(1).py
@@ -0,0 +1,13 @@
+#a,b의 값을 사용자가 입력할 수 있도록 함
+a=input("a를 입력하세요. a=")
+b=input("b를 입력하세요. b=")
+
+#a, b를 정수로 설정
+a=int(a)
+b=int(b)
+
+#정수로 바꾼 a, b 계산 및 출력
+print("a+b=", a+b)
+print("a-b=", a-b)
+print("a*b=", a*b)
+print("a/b=", a/b)
