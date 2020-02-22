5f4eae0043ad38441ef20c169effcf3684abc83c
jiwon824 <59115860+jiwon824@users.noreply.github.com>
2020-02-22 22:38:19 +0900

    Add files via upload

---
+++ python_9day(2).py
@@ -0,0 +1,14 @@
+'''
+Q. 다음에 출력되는 값으로 적절한 것은?
+stuff1 = 'Connect Foundation!'
+print(len(stuff1))
+stuff2 = 'Connect\nFoundation!'
+print(len(stuff2))
+
+답: 19/19
+'''
+
+stuff1 = 'Connect Foundation!'
+print(len(stuff1))
+stuff2 = 'Connect\nFoundation!'
+print(len(stuff2))
