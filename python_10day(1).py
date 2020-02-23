63868ead0d3f71b46d27027f118eabda8c80f066
jiwon824 <59115860+jiwon824@users.noreply.github.com>
2020-02-23 21:26:04 +0900

    Add files via upload

---
+++ python_10day(1).py
@@ -0,0 +1,13 @@
+'''
+Q다음 중 출력되는 값으로 적절한 것은?
+====================================
+words=['Hello', 'World']
+words.append(['Connect', 'Foundation', 'Education'])
+print(len(words))
+====================================
+
+->답: 3 =>리스트 속에 리스트 원소 하나로 들어간다.
+'''
+words=['Hello', 'World']
+words.append(['Connect', 'Foundation', 'Education'])
+print(len(words))
